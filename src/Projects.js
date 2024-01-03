5d76f3c0754a672e72bf9a17cb232894fd70b1c5
AbdulAzizAl-Dalaan <abdulaziz.al-dalaan@wsu.edu>
2024-01-02 16:14:11 -0800

    Added images and adjustments to project page.

--- src/Projects.js
+++ src/Projects.js
@@ -6,6 +6,10 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
+import h2f_img from './images/h2f.png'
+import ext2_img from './images/linux.png'
+import nfl_img from './images/nfl.png'
+import crap_img from './images/wsu.png'
 import "./Component.css"
 
 const projectTitles = [
@@ -15,6 +19,15 @@ const projectTitles = [
   "NFL Prediction Algorithm",
   "Cougar Research Application Portal",
 ];
+
+const projectImages = [
+  h2f_img,
+  '',
+  ext2_img,
+  nfl_img,
+  crap_img
+]
+
 const projectDescriptions = [
   "Winner of 3rd Place in WSUs EECS Capstone Competition. An application designed for the Army National Guard to help improve the time, accuracy, and effectiveness of their Periodic Health Assessment in the form of a multi-page web application.",
   "QuizFeed is a web application that allows users to create and take quizzes. The application is built using React and Firebase.",
@@ -24,7 +37,7 @@ const projectDescriptions = [
 ];
 
 const projectIcons = [
-  ["devicon-javascript-plain", "devicon-nodejs-plain", "devicon-express-original", "devicon-mysql-plain"],
+  ["devicon-javascript-plain", "devicon-nodejs-plain", "devicon-express-original", "devicon-mysql-plain", "devicon-azure-plain"],
   ["devicon-react-original", "devicon-nodejs-plain", "devicon-express-original", "devicon-sqlite-plain"],
   ["devicon-c-plain", "devicon-linux-plain"],
   ["devicon-python-plain", "devicon-pandas-original", "devicon-numpy-original"],
@@ -56,6 +69,7 @@ function Projects() {
           justifyContent: "center",
           flexWrap: "wrap",
           px: 3,
+          pt: 8,
         }}
       >
         {projectTitles.map((item, index) => (
@@ -71,8 +85,12 @@ function Projects() {
             <CardMedia
               component="img"
               alt={item}
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
+              height="230"
+              image={projectImages[index]}
+              // style={{
+              //   objectFit: 'contain',
+              //   objectPosition: 'center' // optional, adjust as needed
+              // }}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
